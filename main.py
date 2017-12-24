de07c4e48f52b4fc7a45d3eea34627f1e2075c95
iGio90 <giovanni.rocca.90@gmail.com>
2017-12-24 02:42:13 +0100

    checkpoint.. it's Santa time... see ya'll after holidays

--- main.py
+++ main.py
@@ -6,45 +6,11 @@ from unicorn import *
 from unicorn.arm_const import *
 from capstone import *
 
-'''
-{'type': 'send', 'payload': '0::::266AF34B2B72A4EAA1D8503531C02114940BA918DC5E1B485BF26D2F6CB7C767'}
-PK:266AF34B2B72A4EAA1D8503531C02114940BA918DC5E1B485BF26D2F6CB7C767
-{'type': 'send', 'payload': '1::::3E0DB818DDC908928F7B670DDC4C3E2FEF0419BFFA160B5E9CF4CE33EC1D119D'}
-SK:3E0DB818DDC908928F7B670DDC4C3E2FEF0419BFFA160B5E9CF4CE33EC1D119D
-attaching blake
-{'type': 'send', 'payload': '2::::B10CD28E5E70EBB8AEF02A25BBCF44F9242083909059D482AF5CDD94B2C58704'}
-PKS:B10CD28E5E70EBB8AEF02A25BBCF44F9242083909059D482AF5CDD94B2C58704
-b2hash: 180000001FAD0B43E3B8FD62A2ABFE8E91BAFC44068B5C712FA118DAD2F75155
-Entering pt5 at 0xefec3b70
-Base at: 0xefae4000
-Image end at: 0xf03b6684
-r0: 0xe08fe178
-r1: 0x16e
-r2: 0xa94dbf60
-r3: 0x17e
-r4: 0xf240589c
-r5: 0xe08ff278
-r6: 0xe08fe2e8
-r7: 0xe08ff668
-r8: 0xa94dbf40
-r9: 0x17e
-r10: 0xf24058bc
-r11: 0x19e
-r12: 0xf411a944
-sp: 0xe08fe168
-pc: 0xefe98f27
-0xe08ff278
-277500019E0009266AF34B2B72A4EAA1D8503531C02114940BA918DC5E1B485BF26D2F6CB7C7673AADA6143D9AE06A39AB7FC0B0BDC7C09DA9E
-DD855987B3CCBD4EA4314B289C11E65289E4D9B1AAF0DABD20A81FF989F700835DB0F49691E4C0AE8D4A37E3BDA4C065F1F50F5A8264CD268DE
-7E93FB6FF909B38F11DFDBEF4D24A462E0F2C00094DA89A0777052015985C889A52DB86A35E67A3B40B78221664641E878FA155861524982A7D
-112638996C3A3771A0DBC34068737F014AEFA00081AB38B4A3F9E692312C7973A69D02E2F25A16DC7E9A1BB1EB81A41AFCB6947DA2AF8C0DA45
-23BE6D9850E1DA333A5A818844DE70053614DFB152E1D1DF08C2B74387B9CAF5EC6AEC7A90D960DD90B3718CDA61FDC184F4CD1A1CD5317CA92
-5483B2B9AD90611A1CF75637A6B22FEBB88C260A6B73027039B4FDB9A17D9F92B9F29043C572F670CA2D8187500BABF5CBCDC010E5FA622A346
-11D9F1ADCF767F18931EC466BAFEFFDED905D37914BAA256AB00C53EC0C4DFCE0926E292BBB2EBAF9FA89C2E797C23175E5033E7F177C035DE2
-5D2DC1E2D86538FB7BAEFAC5FC190183D88BE
-'''
-
-LIBG_ADDRESS = 0xefae4000
+LIBG_ADDRESS = 0xf0f12000
+R4_ADDR = 0xf240755c
+R8_ADDR = 0xa94e29c0
+R12_ADDR = 0xf411a944
+SP_ADDR = 0xe0f7e168
 
 libg = open('libg.so', 'rb').read()
 sp_img = open('sp.bin', 'rb').read()
@@ -59,29 +25,11 @@ enc_memcpy = [0x3EC7F6, 0x3ec8f8, 0x3EC9FA, 0x3ECAFC, 0x3ecc4c, 0x3eabe2, 0x3ec2
               0x3EC49C, 0x3EC59E, 0x3e0c0a, 0x2F5C1E, 0x3E0C4C, 0x3E0D3E, 0x3E0D5A, 0x3E8D22,
               0x3E8DAA, 0x3EE838]
 
-hook_c = False
-
-
-def initialize_registers(mu):
-    sp_addr = 0x100000 + 0x24000
-    r8_addr = 0x200000 + 0x200
-    r4_addr = 0x400000 + 0x500
-    r12_addr = 0x300000 + 0x200
-
-    mu.reg_write(UC_ARM_REG_R0, sp_addr + 0x10)
-    mu.reg_write(UC_ARM_REG_R1, 0x16e)
-    mu.reg_write(UC_ARM_REG_R2, r8_addr + 0x20)
-    mu.reg_write(UC_ARM_REG_R3, 0x17e)
-    mu.reg_write(UC_ARM_REG_R4, r4_addr)
-    mu.reg_write(UC_ARM_REG_R5, sp_addr + 0x1110)
-    mu.reg_write(UC_ARM_REG_R6, sp_addr + 0x180)
-    mu.reg_write(UC_ARM_REG_R7, sp_addr + 0x1500)
-    mu.reg_write(UC_ARM_REG_R8, r8_addr)
-    mu.reg_write(UC_ARM_REG_R9, 0x17e)
-    mu.reg_write(UC_ARM_REG_R10, r4_addr + 0x20)
-    mu.reg_write(UC_ARM_REG_R11, 0x19e)
-    mu.reg_write(UC_ARM_REG_R12, r12_addr)
-    mu.reg_write(UC_ARM_REG_SP, sp_addr)
+# debug things
+dbg = True
+last_jmp = 0x0
+jmp_count = 0
+jmp_dbg = 4
 
 
 def jump(uc, address, size):
@@ -94,19 +42,23 @@ def hook_block(uc, address, size, user_data):
     print(">>> Tracing basic block at 0x%x, block size = 0x%x" % (address, size))
 
 
-def hook_arm(uc, address, size, user_data):
-    ad = address - 0x350000
-    print(">>> Tracing instruction at 0x%x, instruction size = 0x%x" % (ad, size))
-    for i in arm_md.disasm(bytes(uc.mem_read(address, size)), address):
-        print("0x%x:\t%s\t%s" % (i.address - 0x350000, i.mnemonic, i.op_str))
-
-
 def hook_code(uc, address, size, user_data):
-    global hook_c
     ad = address - LIBG_ADDRESS
-    if ad == 0x3B4F26:
-        hook_c = True
-    if hook_c:
+
+    if dbg:
+        global last_jmp
+        global jmp_count
+
+        if last_jmp == 0x0 or ad - last_jmp < 5 or ad + last_jmp > 5:
+            last_jmp = ad
+        else:
+            if jmp_count < jmp_dbg:
+                print(">>> JUMP " + str(jmp_count))
+                jmp_count += 1
+                last_jmp = ad
+            else:
+                print(">>> JUMP BREAK")
+                uc.emu_stop()
         print(">>> Tracing instruction at 0x%x, instruction size = 0x%x" % (ad, size))
         for i in md.disasm(bytes(uc.mem_read(address, size)), address):
             print("0x%x:\t%s\t%s" % (i.address - LIBG_ADDRESS, i.mnemonic, i.op_str))
@@ -114,6 +66,8 @@ def hook_code(uc, address, size, user_data):
     # malloc replace
     if ad == 0x485E0A:
         malloc_replace(uc)
+        uc.reg_write(UC_ARM_REG_R1, 0x64146c41)
+        uc.reg_write(UC_ARM_REG_R2, 0x0)
 
     # payload memcpy
     if ad == 0x3DFC54:
@@ -129,53 +83,8 @@ def hook_code(uc, address, size, user_data):
     if ad in enc_memcpy:
         memcpy_replace(uc)
 
-    # post encryption memcpy call:
-    if ad == 0x3b4f2e:
-        initialize_registers(uc)
-
-    # jfree
-    if ad == 0x485E84 or ad == 0x3b4f32:
-        uc.reg_write(UC_ARM_REG_R0, 0x00)
-
-    # another jfree here. everything is nopped, just set the things up ->
-    if ad == 0x3b4f3a:
-        uc.reg_write(UC_ARM_REG_R4, 0x500000 + 0x200)
-
-    # r6 + 0x14 still point to the hold image when entering 0x3b4f64 :S
-    if ad == 0x3b4f62:
-        uc.mem_write(uc.reg_read(UC_ARM_REG_R6) + 0x14, struct.pack("<I", 0x400000 + 0x500 - 0x9C))
-
-    # patches for message version simplify
-    if ad == 0x3b4f6a:
-        print(hex(struct.unpack("<I", uc.mem_read(uc.reg_read(UC_ARM_REG_R5) + 0x98, 4))[0]))
-    if ad == 0x26a9d4:
-        uc.reg_write(UC_ARM_REG_R0, 0x09)
-
-    # send
-    if ad == 0x0D3266:
-        print("hit send")
-        print_send(uc)
-
-    if ad == 0x3b4f94:
-        uc.reg_write(UC_ARM_REG_R0, LIBG_ADDRESS + 0x331E2E)
-
-    # we can jump straight and skip double recv
-    if ad == 0xFB6DA:
-        uc.reg_write(UC_ARM_REG_R0, 0x400000 + 0x500 - 0x5c)
-        uc.reg_write(UC_ARM_REG_R1, 0x400000 + 0x500 - 0x9c)
-        uc.reg_write(UC_ARM_REG_PC, LIBG_ADDRESS + 0x3A90F9)
-
-    # write enc payload len
-    if ad == 0x3a9150:
-        uc.reg_write(UC_ARM_REG_R0, 0x19e)
-    # these are safely 0
-    if ad == 0x3a915c:
-        uc.reg_write(UC_ARM_REG_R0, 0x00)
-    if ad == 0x3A9194:
-        uc.reg_write(UC_ARM_REG_R5, 0x00)
-    # load msg id
-    if ad == 0x3A918E:
-        uc.reg_write(UC_ARM_REG_R0, 0x2775)
+    if ad == 0x3EC7F6:
+        print_regs(uc)
 
 
 def print_regs(uc):
@@ -220,8 +129,6 @@ def hook_mem_invalid(uc, access, address, size, value, user_data):
     else:
         print(
             ">>> Missing memory is being READ at 0x%x, data size = %u, data value = 0x%x" % (address, size, value))
-        if uc.reg_read(UC_ARM_REG_R0) == 0xd38cac1f:
-            uc.reg_write(UC_ARM_REG_R0, uc.reg_read(UC_ARM_REG_SP) - 0x500)
         print_regs(uc)
         return True
 
@@ -246,6 +153,40 @@ def malloc_replace(uc):
 def memcpy_replace(uc):
     b = uc.mem_read(uc.reg_read(UC_ARM_REG_R1), uc.reg_read(UC_ARM_REG_R2))
     uc.mem_write(uc.reg_read(UC_ARM_REG_R0), bytes(b))
+    print("Copying " + str(hex(uc.reg_read(UC_ARM_REG_R2))) + " from " +
+          str(hex(uc.reg_read(UC_ARM_REG_R1)) + " to " + str(hex(uc.reg_read(UC_ARM_REG_R0)))))
+    hexdump(uc.mem_read(uc.reg_read(UC_ARM_REG_R1), uc.reg_read(UC_ARM_REG_R2)))
+
+
+def patch_img(uc, sp_addr, r4_addr, r8_addr, r12_addr, img, img_offset):
+    rsp_i = struct.unpack("<H", struct.pack("<I", SP_ADDR)[2:4])[0]
+    rr4_i = struct.unpack("<H", struct.pack("<I", R4_ADDR)[2:4])[0]
+    rr8_i = struct.unpack("<H", struct.pack("<I", R8_ADDR)[2:4])[0]
+    rr12_i = struct.unpack("<H", struct.pack("<I", R12_ADDR)[2:4])[0]
+    for i in range(0, len(img)):
+        # read in chunks of 4 bytes
+        b = img[i:i + 4]
+        if len(b) < 4:
+            break
+        b_i = b[2:4]
+        b_i = struct.unpack("<H", b_i)[0]
+        pp = struct.unpack("<I", b)[0]
+        if b_i == rsp_i:
+            ppd = SP_ADDR - pp
+            ppt = sp_addr + ppd
+            uc.mem_write(img_offset + i, struct.pack("<I", ppt))
+        elif b_i == rr4_i:
+            ppd = R4_ADDR - pp
+            ppt = r4_addr + ppd
+            uc.mem_write(img_offset + i, struct.pack("<I", ppt))
+        elif b_i == rr8_i:
+            ppd = R8_ADDR - pp
+            ppt = r8_addr + ppd
+            uc.mem_write(img_offset + i, struct.pack("<I", ppt))
+        elif b_i == rr12_i:
+            ppd = R12_ADDR - pp
+            ppt = r12_addr + ppd
+            uc.mem_write(img_offset + i, struct.pack("<I", ppt))
 
 
 def start():
@@ -271,10 +212,6 @@ def start():
         mu.mem_write(LIBG_ADDRESS + 0x0D326E, bytes.fromhex('00bf00bf00bf00bf00bf'))
         mu.mem_write(LIBG_ADDRESS + 0x38643C, bytes.fromhex('00bf00bf00bf00bf00bf00bf00bf'))
         mu.mem_write(LIBG_ADDRESS + 0x3B4FA8, bytes.fromhex('00bf00bf00bf00bf00bf'))
-        mu.mem_write(LIBG_ADDRESS + 0x1c0160, bytes.fromhex('00bf00bf00bf00bf00bf'))
-        mu.mem_write(LIBG_ADDRESS + 0x3a910c, bytes.fromhex('00bf00bf00bf00bf00bf00bf00bf00bf00bf00bf00bf'))
-        mu.mem_write(LIBG_ADDRESS + 0x3A9138, bytes.fromhex('00bf00bf00bf00bf00bf00bf00bf'))
-
         # nop malloc
         mu.mem_write(LIBG_ADDRESS + 0x485E0A, bytes.fromhex('00bf00bf'))
         # nop memcpy payload
@@ -316,31 +253,6 @@ def start():
         mu.mem_write(LIBG_ADDRESS + 0x3E8BB0, bytes.fromhex('00bf00bf'))
         mu.mem_write(LIBG_ADDRESS + 0x3E8BBA, bytes.fromhex('00bf00bf'))
         mu.mem_write(LIBG_ADDRESS + 0x3E8C3A, bytes.fromhex('00bf00bf'))
-        # nop memcpy's after encr
-        mu.mem_write(LIBG_ADDRESS + 0x3B4F2E, bytes.fromhex('00bf00bf00bf00bf00bf'))
-        # load msg id without reading ptr recursive
-        mu.mem_write(LIBG_ADDRESS + 0x3b4f3a, bytes.fromhex('00bf00bf42f2757000bf00bf'))
-        # nop message version
-        mu.mem_write(LIBG_ADDRESS + 0x26a9d4, bytes.fromhex('00bf'))
-        # hardcode this
-        mu.mem_write(LIBG_ADDRESS + 0x1c176c, bytes.fromhex('006B'))
-        # patch send
-        mu.mem_write(LIBG_ADDRESS + 0x0D3266, bytes.fromhex('00bf00bf'))
-        # patch mem clear loop functions
-        mu.mem_write(LIBG_ADDRESS + 0x3b4f8a, bytes.fromhex('00bf00bf00bf'))
-        mu.mem_write(LIBG_ADDRESS + 0x19ec34, bytes.fromhex('00bf'))
-        mu.mem_write(LIBG_ADDRESS + 0x3b4f9a, bytes.fromhex('00bf00bf'))
-        mu.mem_write(LIBG_ADDRESS + 0x1c014c, bytes.fromhex('00bf00bf00bf00bf00bf00bf00bf00bf'))
-        # sigil found :P
-        # mu.mem_write(LIBG_ADDRESS + 0x4a346c, bytes.fromhex('00bf00bf'))
-        # patch last things before final send
-        mu.mem_write(LIBG_ADDRESS + 0x3A9146, bytes.fromhex('00bf00bf00bf00bf'))
-        # patch payload len calc
-        mu.mem_write(LIBG_ADDRESS + 0x3a9150, bytes.fromhex('00bf00bf'))
-        # patch msg id
-        mu.mem_write(LIBG_ADDRESS + 0x3A918E, bytes.fromhex('00bf00bf'))
-        # patch the check for post login
-        mu.mem_write(LIBG_ADDRESS + 0x3A9194, bytes.fromhex('00bf00bf00bf00bf00bf'))
 
         # map stack pointer
         mu.mem_map(0x100000, 1024 * 512)
@@ -356,11 +268,32 @@ def start():
         mu.mem_map(0x500000, 1024 * 512)
 
         # setup context
-        initialize_registers(mu)
-        mu.reg_write(UC_ARM_REG_LR, LIBG_ADDRESS + 0x3B4F27)
-
         sp_addr = 0x100000 + 0x24000
         r4_addr = 0x400000 + 0x500
+        r8_addr = 0x200000 + 0x200
+        r12_addr = 0x300000 + 0x200
+        # registers
+        mu.reg_write(UC_ARM_REG_R0, sp_addr + 0x10)
+        mu.reg_write(UC_ARM_REG_R1, 0x16e)
+        mu.reg_write(UC_ARM_REG_R2, r8_addr + 0x20)
+        mu.reg_write(UC_ARM_REG_R3, 0x17e)
+        mu.reg_write(UC_ARM_REG_R4, r4_addr)
+        mu.reg_write(UC_ARM_REG_R5, sp_addr + 0x1110)
+        mu.reg_write(UC_ARM_REG_R6, sp_addr + 0x180)
+        mu.reg_write(UC_ARM_REG_R7, sp_addr + 0x1500)
+        mu.reg_write(UC_ARM_REG_R8, r8_addr)
+        mu.reg_write(UC_ARM_REG_R9, 0x17e)
+        mu.reg_write(UC_ARM_REG_R10, r4_addr + 0x20)
+        mu.reg_write(UC_ARM_REG_R11, 0x19e)
+        mu.reg_write(UC_ARM_REG_R12, r12_addr)
+        mu.reg_write(UC_ARM_REG_SP, sp_addr)
+        mu.reg_write(UC_ARM_REG_LR, LIBG_ADDRESS + 0x3B4F27)
+        # I need to patch with a loop... can't find any clean way so I will just code it the worst way
+        patch_img(mu, sp_addr, r4_addr, r8_addr, r12_addr, sp_img, 0x100000)
+        patch_img(mu, sp_addr, r4_addr, r8_addr, r12_addr, extra_high_img, 0x200000)
+        patch_img(mu, sp_addr, r4_addr, r8_addr, r12_addr, extra_high_img_t, 0x300000)
+        patch_img(mu, sp_addr, r4_addr, r8_addr, r12_addr, extra_low_image, 0x400000)
+        # extra patches
         mu.mem_write(mu.reg_read(UC_ARM_REG_R6) + 4, struct.pack("<I", r4_addr + 0x60))
         mu.mem_write(mu.reg_read(UC_ARM_REG_R6) + 8, struct.pack("<I", sp_addr + 0x10))
         mu.mem_write(mu.reg_read(UC_ARM_REG_R6) + 12, struct.pack("<I", r4_addr + 0x40))
@@ -373,40 +306,43 @@ def start():
         print('patched stackpointer is ready to rock :P')
         hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_SP), 400))
 
+        # add hooks
         mu.hook_add(UC_HOOK_CODE, hook_code)
         mu.hook_add(UC_HOOK_MEM_FETCH_UNMAPPED, hook_mem_fetch_unmapped)
         mu.hook_add(UC_HOOK_MEM_READ_UNMAPPED | UC_HOOK_MEM_WRITE_UNMAPPED, hook_mem_invalid)
-        mu.emu_start(LIBG_ADDRESS + 0x3DFB70 | 1, LIBG_ADDRESS + 0x3A9132)
-        print("first stage done")
+
+        # start emulation
+        #mu.emu_start(LIBG_ADDRESS + 0x3DFB70 | 1, LIBG_ADDRESS + 0x3B4F26)
+        mu.emu_start(LIBG_ADDRESS + 0x3DFB70 | 1, LIBG_ADDRESS + 0x3B4F26)
+
         print_regs(mu)
-
-        # now starting the second stage
-        r0 = mu.reg_read(UC_ARM_REG_R0)
-        r1 = mu.reg_read(UC_ARM_REG_R1)
-        arm_mu = Uc(UC_ARCH_ARM, UC_MODE_ARM)
-        arm_mu.mem_map(0x350000, 1024 * 1024 * 512)
-        arm_mu.mem_write(0x350000, libg)
-        arm_mu.reg_write(UC_ARM_REG_R0, r0)
-        arm_mu.reg_write(UC_ARM_REG_R1, r1)
-        arm_mu.reg_write(UC_ARM_REG_LR, 0x00)
-        arm_mu.reg_write(UC_ARM_REG_APSR, 0xFFFFFFFF)
-        # patches
-        arm_mu.mem_write(0x350000 + 0x4a3474, bytes.fromhex('00F020E3'))
-        arm_mu.mem_write(0x350000 + 0x4a347c, bytes.fromhex('00F020E3'))
-        arm_mu.emu_start(0x350000 + 0x4A346C, 0x350000 + 0x4A3488)
-        print("second stage done")
-        print_regs(arm_mu)
-
-        # third stage
-        r0 = arm_mu.reg_read(UC_ARM_REG_R0)
-        r1 = arm_mu.reg_read(UC_ARM_REG_R1)
-        r2 = arm_mu.reg_read(UC_ARM_REG_R2)
-        r12 = arm_mu.reg_read(UC_ARM_REG_R12)
-        mu.reg_write(UC_ARM_REG_R0, r0)
-        mu.reg_write(UC_ARM_REG_R1, r1)
-        mu.reg_write(UC_ARM_REG_R2, r2)
-        mu.reg_write(UC_ARM_REG_R12, r12)
-        mu.emu_start(LIBG_ADDRESS + 0x3A9136 | 1, LIBG_ADDRESS + 0x040EC0)
+        hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R8), 400))
+        print("R0")
+        #hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R0), 128))
+        print("R1")
+        #hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R1), 128))
+        print("R2")
+        #hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R2), 128))
+        print("R3")
+        #hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R3), 128))
+        print("R4")
+        #hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R4), 128))
+        print("R5")
+        hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R5), 128))
+        print("R6")
+        #hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R6), 128))
+        print("R7")
+        hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R7), 128))
+        print("R8")
+        hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R8), 128))
+        print("R9")
+        hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R9), 128))
+        print("R10")
+        hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R10), 128))
+        print("R12")
+        hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_R12), 128))
+        print("SP")
+        hexdump(mu.mem_read(mu.reg_read(UC_ARM_REG_SP), 128))
 
     except UcError as e:
         print("ERROR: %s" % e)
